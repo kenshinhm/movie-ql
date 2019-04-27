8981f683a13cc0774eb0bb46753713dbfe1a6af5
kenshinhm <kenshinhm@naver.com>
2019-04-27 20:18:23 +0900

    final

--- graphql/db.js
+++ graphql/db.js
@@ -1,40 +1,16 @@
-let movies = [
-    {
-        id: 0,
-        name: 'jake',
-        score: 3
-    },
-    {
-        id: 1,
-        name: 'starwars',
-        score: 4
-    },
-];
+import fetch from 'node-fetch';
 
-export const getMovies = () => movies;
+const API_URL = 'https://yts.am/api/v2/list_movies.json';
 
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-};
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = `${API_URL}?`;
+    if (limit > 0) {
+        REQUEST_URL += `limit=${limit}`;
     }
-
-};
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
+    if (rating > 0) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+    return fetch(REQUEST_URL)
+        .then(response => response.json())
+        .then(json => json.data.movies)
 };
